9fb8fe666012b454f218a20a19e9f9563e716395
Brady Underwood <underwood.brady@gmail.com>
2023-11-09 13:06:58 -0700

    frontend setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,17 +2,20 @@ use axum::{
     routing::get, Router,
 };
 use std::net::SocketAddr;
+use axum_error::Result;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(index));
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(index)).layer(CorsLayer::very_permissive());
     let address = SocketAddr::from(([0,0,0,0],8000));
-    axum::Server::bind(&address)
+
+    Ok(axum::Server::bind(&address)
     .serve(app.into_make_service())
     .await
-    .unwrap();
+    .unwrap())
 }
 
 async fn index()-> String{
-    format!("Hello, world!")
+    format!("hi!")
 }
---
+++ frontend/src/routes/+page.ts
@@ -0,0 +1,7 @@
+import type { PageLoad } from "./$types";
+
+const backendURL = 'http://0.0.0.0:8000'
+
+export const load: PageLoad = async () => {
+    return {}
+}
